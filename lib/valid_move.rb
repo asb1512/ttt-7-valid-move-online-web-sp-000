ac2f3a181795ae4e81bdd5ec26f3f1c373dafc19
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-05 19:28:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,8 @@
 def valid_move?(board, index)
   if index >= 0 && index <= 8
     true
-  elsif position_taken?(board, index) == false
-    true
+  elsif position_taken?(board, index) == true
+    false
   end
 end
 
